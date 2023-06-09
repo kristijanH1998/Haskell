2b49855d5166d5b9ee4c4a9a4887394c1da0ef59
kristijanH1998 <kristijan.hornung1@gmail.com>
2023-06-08 22:48:47 -0700

    ch5

--- ch5.hs
+++ ch5.hs
@@ -1,13 +1,17 @@
-
+import Prelude hiding (replicate)
 --5.2
 grid :: Int -> Int -> [(Int,Int)]
 grid m n = [(x,y) | x <- [0..m], y <- [0..n]]
 --5.3
 square :: Int -> [(Int,Int)]
 square n = [(x,y) | (x,y) <- grid n n, x /= y]
+--5.4
+replicate :: Int -> a -> [a]
+replicate a b = [b | _ <- [1..a]]
 main = do
     --5.1
     print $ [sum([x^2 | x <- [1..100]])]
+    --
     print $ grid 1 2
     print $ square 2
-    
+    print $ replicate 3 True

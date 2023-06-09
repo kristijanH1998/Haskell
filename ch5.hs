210185ef28e9df7df752dc608b32a24d53b5694e
kristijanH1998 <kristijan.hornung1@gmail.com>
2023-06-08 22:34:45 -0700

    ch5

--- ch5.hs
+++ ch5.hs
@@ -2,10 +2,12 @@
 --5.2
 grid :: Int -> Int -> [(Int,Int)]
 grid m n = [(x,y) | x <- [0..m], y <- [0..n]]
-
+--5.3
+square :: Int -> [(Int,Int)]
+square n = [(x,y) | (x,y) <- grid n n, x /= y]
 main = do
     --5.1
     print $ [sum([x^2 | x <- [1..100]])]
     print $ grid 1 2
-
-    --
+    print $ square 2
+    

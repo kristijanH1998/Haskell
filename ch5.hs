c81b8b103b3c5cb64956798e81878ee7b6abdae0
kristijanH1998 <kristijan.hornung1@gmail.com>
2023-06-09 21:52:27 -0700

    ch5

--- ch5.hs
+++ ch5.hs
@@ -16,6 +16,14 @@ factors :: Int -> [Int]
 factors n = [x | x <- [1..n], n `mod` x == 0]
 perfects :: Int -> [Int]
 perfects x = [n | n <- [1..x], n == sum(tail(reverse(factors n)))]
+--5.8
+find :: Eq a => a -> [(a,b)] -> [b]
+find k t = [v | (k', v) <- t, k == k']
+positions :: Eq a => a -> [a] -> [Int]
+positions x xs = [i | (x', i) <- zip xs [0..], x == x']
+
+mypositions :: Eq a => a -> [a] -> [Int]
+mypositions x xs = find x [(y,z) | (y,z) <- zip xs [0..]]
 
 main = do
     --5.1
@@ -30,3 +38,8 @@ main = do
     print $ [(x,y) | x <- [1,2], y <- [3,4]]
     print $ concat [[(1,y) | y <- [3,4]], [(2,z) | z <- [3,4]]]
     print $ concat [[(x,y) | y <- [3,4]] | x <- [1,2]]
+    --5.8
+    print $ positions False [True, False, True, False]
+    print $ find 'b' [('a',1), ('b',2),('c',3),('b',4)]
+    print $ mypositions False [True, False, True, False]
+    print $ mypositions 2 [1,1,3,4,5,2,2,8,2]

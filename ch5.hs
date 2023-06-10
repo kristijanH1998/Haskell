f48fa844040a1cd9866069f902861ab7b6b7c271
kristijanH1998 <kristijan.hornung1@gmail.com>
2023-06-10 01:22:55 -0700

    ch5 ex10 final version

--- ch5.hs
+++ ch5.hs
@@ -42,8 +42,33 @@ shift n c | isLower c = int2letL ((let2int c + n) `mod` 26)
           | otherwise = c
 encode :: Int -> String -> String
 encode n xs = [shift n x | x <- xs]
-
-
+table :: [Float]
+table = [8.1,1.5,2.8,4.2,12.7,2.2,2.0,6.1,7.0,
+        0.2,0.8,4.0,2.4,6.7,7.5,1.9,0.1,6.0,
+        6.3,9.0,2.8,1.0,2.4,0.2,2.0,0.1]
+percent :: Int -> Int -> Float
+percent n m = (fromIntegral n / fromIntegral m) * 100
+letters :: String -> Int
+letters xs = length [x | x <- xs, isAlpha x]
+freqs :: String -> [Float]
+lowers :: String -> Int
+lowers xs = length [x | x <- xs, x >= 'a' && x <= 'z']
+count :: Char -> String -> Int
+count x xs = length [x' | x' <- xs, x == x']
+freqs xs = [percent (count x xs') n | x <- ['a'..'z']]
+           where xs' = map toLower xs
+                 n = letters xs  
+chisqr :: [Float] -> [Float] -> Float
+chisqr os es = sum [((o-e) ^ 2) / e | (o,e) <- zip os es]
+rotate :: Int -> [a] -> [a]
+rotate n xs = drop n xs ++ take n xs
+crack :: String -> String
+crack xs = encode (-factor) xs
+    where
+        factor = head(positions(minimum chitab) chitab)
+        chitab = [chisqr (rotate n table') table | n <- [0..25]]
+        table' = freqs xs
+--
 main = do
     --5.1
     print $ [sum([x^2 | x <- [1..100]])]
@@ -68,3 +93,6 @@ main = do
     print $ shift 3 'a'
     print $ shift 3 'A'
     print $ encode 3 "haskell is fun"
+    print $ crack "kdvnhoo lv ixq"
+    print $ crack "vscd mywzboroxcsyxc kbo ecopev"
+    print $ crack (encode 3 "my name is Kristijan")

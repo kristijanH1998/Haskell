c042cb35b93767d950f1bf0e6ed70872b67424ce
kristijanH1998 <kristijan.hornung1@gmail.com>
2023-06-13 01:48:53 -0700

    ch6 ex 5

--- ch6.hs
+++ ch6.hs
@@ -1,7 +1,7 @@
 import Prelude hiding ((^))
 --6.1
---if applied to negative numbers, recursion of fac never terminates because its negative argument is repeatedly decreased by 1 
---and never reaches 0
+-- if applied to negative numbers, recursion of fac never terminates because its negative argument is repeatedly decreased by 1 
+-- and never reaches 0
 fac :: Int -> Int
 fac 0 = 1
 fac n | n > 0   = n * fac (n-1)
@@ -31,3 +31,12 @@ main = do
     print $ sumdown(-2)
     print $ 2 ^ 3
     print $ euclid 28 6
+    {- 6.5
+    length [1,2,3] evaluation = {applying length} 1 + length [2,3] = {applying length} 1 + (1 + length [3]) = {applying length}
+    1 + (1 + (1 + length [])) = {applying length} 1 + (1 + (1 + 0)) = {applying +} 3
+    drop 3 [1,2,3,4,5] evaluation = {applying drop} drop 2 [2,3,4,5] = {applying drop} drop 1 [3,4,5] = {applying drop} drop 0 [4,5]
+    = {applying drop} [4,5]
+    init [1,2,3] evaluation = {applying init} 1 : init[2,3] = {applying init} 1 : 2 : init [3] = {applying init} 1 : 2 : [] = 
+    {list notation} [1,2]
+    -}
+    

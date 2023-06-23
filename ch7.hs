b6fbe0ea3e73b86b96e0c245e240a8ed12709015
kristijanH1998 <kristijan.hornung1@gmail.com>
2023-06-23 03:15:48 -0700

    ch7 ex5

--- ch7.hs
+++ ch7.hs
@@ -1,4 +1,4 @@
-import Prelude hiding (map, filter, all, any, takeWhile, dropWhile)
+import Prelude hiding (map, filter, all, any, takeWhile, dropWhile, curry)
 
 map :: (a -> b) -> [a] -> [b]
 map f [] = []
@@ -6,13 +6,11 @@ map f (x:xs) = f x : map f xs
 
 filter :: (a -> Bool) -> [a] -> [a]
 filter p xs = [x | x <- xs, p x]
-
 --7.1
 mapFilt :: (a -> b) -> (a -> Bool) -> [a] -> [b]
 mapFilt f p [] = []
 --[f x | x <- xs, p x] can be expressed with map and filter as:
 mapFilt f p (xs) = map f (filter p xs)
-
 --7.2
 --a)
 all :: (a -> Bool) -> [a] -> Bool
@@ -35,10 +33,17 @@ takeWhile p (x:xs) | p x = x : takeWhile p xs
 dropWhile :: (a -> Bool) -> [a] -> [a]
 dropWhile p [] = []
 dropWhile p (x:xs) = if p x then dropWhile p xs else x:xs
-
 --7.3
 mapFldr f = foldr (\x xs -> f x : xs) []
 filterFldr p = foldr (\x xs -> if p x then x:xs else xs) []
+--7.4
+dec2int :: [Int] -> Int
+dec2int = foldl (\x y -> y + 10*x) 0
+--7.5
+curry :: ((a, b) -> c) -> (a -> (b -> c))
+curry f = \x y -> f(x,y)  
+uncurry :: (a -> b -> c) -> ((a, b) -> c)
+uncurry f = \(x,y) -> f x y
 
 main = do
     print $ map (+1) $ filter even [1..10]
@@ -52,3 +57,4 @@ main = do
     print $ dropWhile (even) [4,6,8,9,11,15]
     print $ mapFldr (*2) [1,2,3,4,5]
     print $ filterFldr (<10) [11,21,2,4,15,22,1] 
+    print $ dec2int [7,5,2,7]

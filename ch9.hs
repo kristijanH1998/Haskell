05cc66ad794be25dd0e38a0af60d716f07ee7320
kristijanH1998 <kristijan.hornung1@gmail.com>
2023-07-10 16:35:28 -0700

    ch9 ex4 partial

--- ch9.hs
+++ ch9.hs
@@ -1,4 +1,5 @@
 import Prelude hiding ()
+import Data.Typeable
 data Op = Add | Sub | Mul | Div
 instance Show Op where
     show Add = "+"
@@ -52,7 +53,37 @@ rmFirstOcc v (x:xs) = if (x == v) then xs else [x] ++ (rmFirstOcc v xs)
 isChoice :: Eq a => [a] -> [a] -> Bool
 isChoice _ [] = False
 isChoice [] (y:ys) = True
-isChoice (x:xs) (y:ys) = if rmFirstOcc x (y:ys) == (y:ys) then False else isChoice xs (y:ys)  
+isChoice (x:xs) (y:ys) = if rmFirstOcc x (y:ys) == (y:ys) then False else isChoice xs (y:ys)
+  
+solution :: Expr -> [Int] -> Int -> Bool
+solution e ns n = 
+    elem (values e) (choices ns) && eval e == [n]
+split :: [a] -> [([a],[a])]
+split []        = []
+split [_]       = []
+split (x:xs)    = ([x],xs) : [(x:ls,rs) | (ls,rs) <- split xs]
+exprs :: [Int] -> [Expr]
+exprs []    = []
+exprs [n]   = [Val n]
+exprs ns    = [e | (ls,rs) <- split ns,
+                   l       <- exprs ls,
+                   r       <- exprs rs,
+                   e       <- combine l r]
+combine :: Expr -> Expr -> [Expr]
+combine l r = [App o l r | o <- ops]
+ops :: [Op]
+ops = [Add,Sub,Mul,Div]
+solutions :: [Int] -> Int -> [Expr]
+solutions ns n = [e | ns' <- choices ns, e <- exprs ns', eval e == [n]]
+{- 9.3
+Generalising the function split to also return pairs containing the empty list would cause non-termination of exprs function because
+its recursive calls would no longer be guaranteed to reduce the length of the list.
+-}
+--9.4
+allExprs :: [Int] -> [Expr]
+allExprs xs = [exp | choice <- choices xs, exp <- exprs choice]
+successfuls :: [Expr] -> [Expr]
+successfuls exps = [sucExp | sucExp <- exps, eval sucExp >= [0]]  
 
 main = do
     --print $ show (App Add (Val 1) (App Mul (Val 2) (Val 3)))
@@ -64,3 +95,12 @@ main = do
     print $ choices [1,2,3]
     print $ rmFirstOcc 2 [1,2,2,2,3,4,5,2]
     print $ isChoice [3,1,2] [1,2,3]
+    print $ solution (App Mul (App Add (Val 1) (Val 50)) (App Sub (Val 25) (Val 10))) [1,3,7,10,25,50] 765
+    print $ split [1,2,3,4]
+    --print $ solutions [1,3,7,10,25,50] 765
+--9.4
+    print $ length (allExprs [1,3,7,10,25,50])
+    print $ length (successfuls (allExprs [1,3,7,10,25,50]))
+    --print $ round (fromIntegral (head (eval (head (allExprs [1,3,7,10,25,50]))))) -- >= 1 && 
+                                --head (eval (head (allExprs [1,3,7,10,25,50]))) >= 0
+    --print $ (round (fromIntegral ((head (eval (App Add (Val 1) (App Mul (Val 2) (Val 3))))))))

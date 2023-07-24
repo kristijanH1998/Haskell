b278b81f2e3adf2f10516771af66037225ede6bf
kristijanH1998 <kristijan.hornung1@gmail.com>
2023-07-23 19:50:13 -0700

    minimax finished

--- ch11_tic-tac-toe.hs
+++ ch11_tic-tac-toe.hs
@@ -49,9 +49,84 @@ interleave :: a -> [a] -> [a]
 interleave x [] = []
 interleave x [y] = [y]
 interleave x (y:ys) = y : x : interleave x ys
+valid :: Grid -> Int -> Bool
+valid g i = 0 <= i && i < size^2 && concat g !! i == B
+move :: Grid -> Int -> Player -> [Grid]
+move g i p = 
+  if valid g i then [chop size (xs ++ [p] ++ ys)] else []
+  where (xs, B:ys) = splitAt i (concat g)
+chop :: Int -> [a] -> [[a]]
+chop n [] = []
+chop n xs = take n xs : chop n (drop n xs)
+getNat :: String -> IO Int
+getNat prompt = do putStr prompt
+                   xs <- getLine
+                   if xs /= [] && all isDigit xs then
+                      return (read xs)
+                   else 
+                      do putStrLn "ERROR: Invalid number"
+                         getNat prompt
+prompt :: Player -> String
+prompt p = "Player " ++ show p ++ ", enter your move: "
+data Tree a = Node a [Tree a]
+              deriving Show
+gametree :: Grid -> Player -> Tree Grid
+gametree g p = Node g [gametree g' (next p) | g' <- moves g p]
+moves :: Grid -> Player -> [Grid]
+moves g p 
+  | won g = []
+  | full g = []
+  | otherwise = concat [move g i p | i <- [0..((size^2)-1)]] 
+prune :: Int -> Tree a -> Tree a
+prune 0 (Node x _) = Node x []
+prune n (Node x ts) = Node x [prune (n-1) t | t <- ts]
+depth :: Int
+depth = 9
+minimax :: Tree Grid -> Tree (Grid,Player)
+minimax (Node g [])
+  | wins O g = Node (g,O) []
+  | wins X g = Node (g,X) []
+  | otherwise = Node (g,B) []
+minimax (Node g ts)
+  | turn g == O = Node (g, minimum ps) ts'
+  | turn g == X = Node (g, maximum ps) ts'
+                  where
+                    ts' = map minimax ts
+                    ps = [p | Node (_,p) _ <- ts']
+bestmove :: Grid -> Player -> Grid
+bestmove g p = head [g' | Node (g',p') _ <- ts, p' == best]
+               where
+                  tree = prune depth (gametree g p)
+                  Node (_,best) ts = minimax tree
+type Pos = (Int,Int)
+goto :: Pos -> IO ()
+goto (x,y) = putStr ("\ESC[" ++ show y ++ ";" ++ show x ++ "H")
+cls :: IO ()
+cls = putStr "\ESC[2J"
+main' :: IO ()
+main' = do hSetBuffering stdout NoBuffering
+           play empty O
+play :: Grid -> Player -> IO ()
+play g p = do cls
+              goto (1,1)
+              putGrid g
+              play' g p
+play' :: Grid -> Player -> IO ()
+play' g p
+  | wins O g = putStrLn "Player O wins!\n"
+  | wins X g = putStrLn "Player X wins!\n"
+  | full g   = putStrLn "It's a draw!\n"
+  | p == O   = do i <- getNat (prompt p)
+                  case move g i p of
+                    [] -> do putStrLn "ERROR: Invalid move"
+                             play' g p
+                    [g'] -> play g' (next p)
+  | p == X   = do putStr "Player X is thinking..."
+                  (play $! (bestmove g p)) (next p)                  
 
 main = do
     print $ showRow [O,B,X]
     --print $ foldr1 (zipWith (++)) (interleave (replicate 3 "|") (map showPlayer [O,B,X]))
     --print $ (interleave (replicate 3 "|") (map showPlayer [O,B,X]))
     --print $ (zipWith (++)) ["   "," O ","   "] ["|","|","|"]
+    main'
